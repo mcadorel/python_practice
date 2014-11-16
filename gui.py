2d4c968cd28e413b40b21e804c51e4e156b94e84
Maël Cadorel <mael.cadorel@laposte.net>
2014-11-16 14:39:15 +0100

    Functional toggle_fullscreen (bound to F11)

--- gui.py
+++ gui.py
@@ -13,6 +13,9 @@ class GUI(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.title('GUI')
+        self.fullscreen = False
+
+        # MENU
         self.menuStrip = Menu(self)
         self.menuStrip.add_command(label="Hello", command=self.placeholder)
         self.menuStrip.add_command(label="Quit", command=self.close)
@@ -20,15 +23,37 @@ class GUI(Tk):
 
         # BINDINGS
         self.bindings = {
-            '<Up>'  :lambda event:print('Up!'),
-            '<Down>':lambda event:print('Down!'),
-            '<Left>'  :lambda event:print('Left!'),
-            '<Right>':lambda event:print('Right!')
+            '<Up>'      : lambda event: print('Up!'),
+            '<Down>'    : lambda event: print('Down!'),
+            '<Left>'    : lambda event: print('Left!'),
+            '<Right>'   : lambda event: print('Right!'),
+            '<F11>'     : self.toggle_fullscreen
             }
         
         # Bind keys
         for (key, action) in self.bindings.items():
-            self.bind(key, action) # TODO (not working)
+            self.bind(key, action)
+
+    def toggle_fullscreen(self, event):
+        if not self.fullscreen:
+            self.sizeBuffer = (self.winfo_height(), self.winfo_width())
+            self.geometry(
+                "{0}x{1}+0+0".format(
+                    self.winfo_screenwidth(),
+                    self.winfo_screenheight()
+                    )
+                )
+            self.overrideredirect(1)
+            self.focus_set()
+            self.fullscreen = True
+        else:
+            self.overrideredirect(0)
+            self.geometry(
+                "{0}x{1}+0+0".format(
+                    *self.sizeBuffer
+                    )
+                )
+            self.fullscreen = False
 
     def placeholder(self):
         print('Blargh!')
