3ee6177e6c6b8026eb0e81a6e732015e7be28f8f
Maël Cadorel <mael.cadorel@laposte.net>
2014-11-17 18:29:40 +0100

    Refactoring : class Data has its own file

--- app.py
+++ app.py
@@ -4,24 +4,16 @@
 
 """ Random snippets for practice. """
 
-
 from grid import Grid, Screen
 from gridViewer import GridViewer
 from gui import GUI
-
-
-class Data(dict):
-    """ Used for application-layer storage of resources. """
-    
-    def __init__(self):
-        dict.__init__(self)
         
 
 class App(object):
     """ Main executable. Composed of
     - Data
     - GUI
-    - GridViewer widget. """
+    - widgets. """
     
     def __init__(self):
         pass
---
+++ data.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8
+# mael.cadorel@laposte.net
+
+
+""" Random snippets for practice. """
+
+
+class Data(dict):
+    """ Used for application-layer storage of resources. """
+        
+    def __init__(self):
+        dict.__init__(self)
